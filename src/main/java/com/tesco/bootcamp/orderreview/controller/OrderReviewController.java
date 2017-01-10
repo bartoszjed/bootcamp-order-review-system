d0e2e32637c0dff4214ee2dd84a047e306aa2008
Rich <richjdurley@gmail.com>
2017-01-10 15:47:06 +0000

    added customer service cclass

---
+++ src/main/java/com/tesco/bootcamp/orderreview/controller/OrderReviewController.java
@@ -0,0 +1,20 @@
+package com.tesco.bootcamp.orderreview.controller;
+
+import com.tesco.bootcamp.orderreview.service.OrderReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OrderReviewController {
+    
+    @Autowired
+    OrderReviewService orderReviewService;
+
+    @RequestMapping("/order-review")
+    public void getOrdersToReview(Model model, @RequestParam(value="customerID", required=true) String customerID) {
+        model.addAttribute("customerName", orderReviewService.getCustomerName(customerID));
+    }
+}
---
+++ src/main/java/com/tesco/bootcamp/orderreview/service/OrderReviewService.java
@@ -0,0 +1,12 @@
+package com.tesco.bootcamp.orderreview.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderReviewService {
+
+  public String getCustomerName(String customerID) {
+    return "Rich";
+  }
+
+}
