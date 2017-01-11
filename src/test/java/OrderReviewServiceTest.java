dacd164c5737830bf591866a200afd7f698f84ab
Manindra Jha <manindrajha@Manindras-MacBook-Pro.local>
2017-01-11 16:34:50 +0000

    Introduced Order adaptor and new test

--- src/test/java/OrderReviewServiceTest.java
+++ src/test/java/OrderReviewServiceTest.java
@@ -1,8 +1,10 @@
-import com.tesco.bootcamp.orderreview.representations.CustomerOrder;
+import builder.*;
+import com.tesco.bootcamp.orderreview.representations.*;
 import com.tesco.bootcamp.orderreview.service.OrderReviewService;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
@@ -36,7 +38,7 @@ public class OrderReviewServiceTest {
 
 
   }
-/*
+
   @Test
   public void shouldReturnListOfOrdersForGivenCustomerId(){
 
@@ -49,6 +51,29 @@ public class OrderReviewServiceTest {
 
     //Then
     assertTrue(orderReviewService.getOrderList(customerId).size()>=0);
-*/
+
+    //Build List of Products
+    List<Product> productList= new ArrayList<Product>();
+    Product product= ProductBuilder.aProduct().withName("Beans").withCategory("Canned Food").build();
+    productList.add(product);
+    //Build Customer Name
+    CustomerName customerName = CustomerNameBuilder.aCustomerName().withFirstName("Alan").withSurname("Lamb").build();
+
+    //Build Customer
+    Customer customer = CustomerBuilder.aCustomer().withId(1).withCustomerName(customerName).build();
+    //Build Order Destination
+    OrderDestination orderDestination = OrderDestinationBuilder.anOrderDestination().withLatitude(23.50).withLongitude(45.00).build();
+
+    //Build Customer Order
+    CustomerOrderBuilder.aCustomerOrder()
+            .withId("12")
+            .withOrderDateAndTime("10-Jan-2017 15:00:00")
+            .withProductList(productList)
+            .withCustomer(customer)
+            .withOrderDestination(orderDestination)
+            .build();
+
+  }
+
 
 }
