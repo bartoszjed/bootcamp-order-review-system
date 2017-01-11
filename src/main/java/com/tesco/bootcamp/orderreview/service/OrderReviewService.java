b9b1fc965dafb83bc0450cfdb4e3059decfeaa3f
Bartosz Jedrzejczak <bartoszjed@gmail.com>
2017-01-11 14:10:33 +0000

    customerURL bean added

--- src/main/java/com/tesco/bootcamp/orderreview/service/OrderReviewService.java
+++ src/main/java/com/tesco/bootcamp/orderreview/service/OrderReviewService.java
@@ -1,6 +1,8 @@
 package com.tesco.bootcamp.orderreview.service;
 
 import com.tesco.bootcamp.orderreview.representations.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,18 +12,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-@Configuration
 public class OrderReviewService {
 
-    @Value("${customerAPi.url}")
-    private String customerAPiURi;
+    private final String customerApiURi;
+
+    @Autowired
+    public OrderReviewService(@Qualifier("customerServiceURL") String url){
+        this.customerApiURi = url;
+    }
 
 
     public String getCustomerName(String customerID) {
         RestTemplate restTemplate = new RestTemplate();
         try {
             ResponseEntity<Customer> collectRequestResult = restTemplate.exchange(
-                    customerAPiURi+"/customer?login=" + customerID+"&password=Password!23",
+                    customerApiURi+"/customer?login=" + customerID+"&password=Password!23",
                     HttpMethod.GET,
                     null,
                     new ParameterizedTypeReference<Customer>() {
--- src/test/java/OrderReviewServiceShould.java
+++ src/test/java/OrderReviewServiceShould.java
@@ -1,6 +1,8 @@
 import com.tesco.bootcamp.orderreview.service.OrderReviewService;
 import junit.framework.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -10,7 +12,7 @@ public class OrderReviewServiceShould {
   @Test
   public void returnCustomerNameWhenCustomerIDIsPassed() {
     //Given
-    OrderReviewService orderReviewService = new OrderReviewService();
+    OrderReviewService orderReviewService = new OrderReviewService("");
     //When
     String customerID="123456";
     //Then
