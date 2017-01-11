05f557952bc8c0eb969bc822df304e81be1a3430
maheshnakum <mahesh>
2017-01-11 13:10:14 +0000

    create configuration for customer and order apis

--- src/main/java/com/tesco/bootcamp/orderreview/service/OrderReviewService.java
+++ src/main/java/com/tesco/bootcamp/orderreview/service/OrderReviewService.java
@@ -1,6 +1,8 @@
 package com.tesco.bootcamp.orderreview.service;
 
 import com.tesco.bootcamp.orderreview.representations.Customer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +10,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Configuration
 public class OrderReviewService {
+
+    @Value("${customerAPi.url}")
+    private String customerAPiURi;
 
 
     public String getCustomerName(String customerID) {
         RestTemplate restTemplate = new RestTemplate();
+        //String url = "http://customers.dev-environment.tesco.codurance.io:8080/customer?login=";
         try {
             ResponseEntity<Customer> collectRequestResult = restTemplate.exchange(
-                    "http://customers.dev-environment.tesco.codurance.io:8080/customer?login=KayleighJohnston1&password=Password!23",
+                    customerAPiURi+"/customer?login=" + customerID+"&password=Password!23",
                     HttpMethod.GET,
                     null,
                     new ParameterizedTypeReference<Customer>() {
