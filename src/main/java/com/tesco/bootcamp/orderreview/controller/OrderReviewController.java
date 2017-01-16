017672f20e1eb041626f9af797a5a2db6be9e877
Bartosz Jedrzejczak <bartoszjed@gmail.com>
2017-01-16 20:50:19 +0000

    Hardcoded order status "Picked" replaced with a reference to orderReviewService.getOrderStatus(orderId).getStatus()

--- src/main/java/com/tesco/bootcamp/orderreview/controller/OrderReviewController.java
+++ src/main/java/com/tesco/bootcamp/orderreview/controller/OrderReviewController.java
@@ -3,6 +3,7 @@ package com.tesco.bootcamp.orderreview.controller;
 import com.tesco.bootcamp.orderreview.representations.Customer;
 import com.tesco.bootcamp.orderreview.representations.CustomerOrder;
 import com.tesco.bootcamp.orderreview.representations.CustomerOrderData;
+import com.tesco.bootcamp.orderreview.representations.OrderStatus;
 import com.tesco.bootcamp.orderreview.service.OrderReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,33 +17,35 @@ import java.util.List;
 @Controller
 public class OrderReviewController {
 
-  private final OrderReviewService orderReviewService;
+    private final OrderReviewService orderReviewService;
 
-  @Autowired
-  public OrderReviewController(OrderReviewService orderReviewService) {
-    this.orderReviewService = orderReviewService;
-  }
-
-  @RequestMapping("/order-review")
-  public void getOrdersToReview(Model model,
-      @RequestParam(value = "loginID", required = true) String loginID) {
-
-    Customer customer = orderReviewService.getCustomerName(loginID);
-
-    int customerID = customer.getId();
-    String customerName = customer.getCustomerName().getFullName();
-
-    model.addAttribute("customerName", customerName);
-
-    //model.addAttribute("customerOrders", orderReviewService.getOrderList(customerID));
-
-    // Concatenate the Orders with their respective delivery status.
-    List<CustomerOrderData> customerOrderData = new ArrayList();
-    for (CustomerOrder i :orderReviewService.getOrderList(customerID) ) {
-      customerOrderData.add(new CustomerOrderData(i.getId(),i.getProducts(),i.getOrderDateTime(),"Picked"));
+    @Autowired
+    public OrderReviewController(OrderReviewService orderReviewService) {
+        this.orderReviewService = orderReviewService;
     }
 
-    model.addAttribute("customerOrderData", customerOrderData);
+    @RequestMapping("/order-review")
+    public void getOrdersToReview(Model model,
+                                  @RequestParam(value = "loginID", required = true) String loginID) {
+
+        Customer customer = orderReviewService.getCustomerName(loginID);
+
+        int customerID = customer.getId();
+        String customerName = customer.getCustomerName().getFullName();
+
+        model.addAttribute("customerName", customerName);
+
+        //model.addAttribute("customerOrders", orderReviewService.getOrderList(customerID));
+
+        // Concatenate the Orders with their respective delivery status.
+        List<CustomerOrderData> customerOrderData = new ArrayList();
+        for (CustomerOrder i : orderReviewService.getOrderList(customerID)) {
+            String orderId = i.getId();
+            String orderStatus = orderReviewService.getOrderStatus(orderId).getStatus();
+            customerOrderData.add(new CustomerOrderData(orderId, i.getProducts(), i.getOrderDateTime(), orderStatus));
+        }
+
+        model.addAttribute("customerOrderData", customerOrderData);
 
 
     }
