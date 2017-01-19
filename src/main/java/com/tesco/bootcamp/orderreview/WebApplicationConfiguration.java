b89642b441528a1312680c41eb9a439d5d0a5f75
Manindra Jha <manindra_jha@yahoo.com>
2017-01-19 10:50:40 +0000

    added time out

--- src/main/java/com/tesco/bootcamp/orderreview/WebApplicationConfiguration.java
+++ src/main/java/com/tesco/bootcamp/orderreview/WebApplicationConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -48,7 +50,14 @@ public class WebApplicationConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
+        return factory;
     }
 
 }
